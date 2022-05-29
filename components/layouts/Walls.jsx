cc09da31688a667bed1036a4b192d64ae38fe427
decefemz <36079094+decefemz@users.noreply.github.com>
2022-05-29 02:02:20 +0200

    added animations

--- components/layouts/Walls.jsx
+++ components/layouts/Walls.jsx
@@ -8,13 +8,14 @@ import * as vars from '../../vars'
 export default function Walls(props) {
   const [data, setData] = useState(null)
   const isLogged = useContext(LoginContext)
-
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     setData(props.walls)
+    setTimeout(() => setLoading(false), 1000)
   }, [props.walls]
   )
 
-  if (data == null) return <Loading minHeight='50vh' />
+  if (loading) return <Loading minHeight='50vh' />
 
   if (!data.length) {
     return <NoWall loggedIn={isLogged?.username == props.wallOwner} />
