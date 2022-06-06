6d92fb2471a6fe3e5ff413df3855715b9df3e424
decefemz <36079094+decefemz@users.noreply.github.com>
2022-06-06 08:58:21 +0200

    added adsense

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1182288742115330"
+            crossorigin="anonymous"></script>
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
